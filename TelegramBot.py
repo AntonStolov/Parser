c09ea74273070680a18e5f1fa4ff71cd93073be7
AntonStolov <Stoilovskyi.anton@gmail.com>
2017-09-13 02:04:05 +0200

    working like a deamon, added answer for \help message

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,12 +1,10 @@
 import requests
 import OlxParser
+import time
 
 token = '385017512:AAHrISO0n6qaEcYbZq9utTdT8PqWLZw3Y7A'
 URL ='https://api.telegram.org/bot' + token + '/'
-new_heders = OlxParser.parser()
-
-
-
+triger = False
 
 def get_updates():
     url = URL + 'getupdates'
@@ -21,23 +19,38 @@ def get_message():
                'text': message_text}
     return message
 
+def get_update_id():
+    data = get_updates()
+    # first_update_id = data['result'][-2]['update_id']
+    update_id = data['result'][-1]['update_id']
+    # if first_update_id == update_id:
+    #     return True
+    return update_id
+
+
 def send_message(chat_id, text = 'jast a second...'):
     url = URL + 'sendmessage?chat_id=' + str(chat_id) + '&text=' + text
     print (url)
     requests.get(url)
 
 
-# def message_creator(heders):
-if new_heders != None:
-    for i in range(len(new_heders)):
-        message = '    ' + new_heders[i]['text'] + '   ' + new_heders[i]['link']
-        send_message(get_message()['chat_id'], message)
-        print (message)
 
-else:
-    message = 'sorry, but I can`t find new position'
-    send_message(get_message()['chat_id'], message)
+if __name__ == "__main__":
+    # def message_creator(heders):
+    first_update_id = get_update_id()
 
+    while True:
+        new_heders = OlxParser.parser()
+        if new_heders != None:
+            for i in range(len(new_heders)):
+                message = '    ' + new_heders[i]['text'] + '   ' + new_heders[i]['link']
+                send_message(get_message()['chat_id'], message)
+                print (message)
+        print(get_update_id())
 
+        time.sleep(1)
 
-
+        if first_update_id != get_update_id():
+            if get_message()['text'] == '/help':
+                send_message(get_message()['chat_id'], 'take instruction')
+                first_update_id = get_update_id()
