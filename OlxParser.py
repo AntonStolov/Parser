14b39818ca828a0d0917dfb472aac0bae3e2030d
AntonStolov <Stoilovskyi.anton@gmail.com>
2017-09-15 18:04:50 +0200

    firstjson.py deleted

--- OlxParser.py
+++ OlxParser.py
@@ -10,13 +10,13 @@ import pracuj
 
 def parser():
     triger = False
-    def trade_spider(max_pages, url, tag, classCss,):
+    def trade_spider(max_pages, url, tag, class_css, ):
         page = 0
         while page <= max_pages:
             source_code = requests.get(url)
             plain_text = source_code.text
             soup = BeautifulSoup(plain_text, "html.parser")
-            allLinks = soup.findAll(tag, {'class': classCss})
+            allLinks = soup.findAll(tag, {'class': class_css})
             headers = []
             for link in allLinks:
                 headers.append({'text': link.strong.string, 'link': link.a['href']})
@@ -26,38 +26,38 @@ def parser():
 
 
     url = 'https://www.olx.pl/praca/informatyka/warszawa/?search%5Bdistrict_id%5D=351&search%5Bfilter_enum_type%5D%5B0%5D=practice'
-    headersInterships = trade_spider(0, url, 'h3', 'x-large lheight20 margintop5')
+    headers_interships = trade_spider(0, url, 'h3', 'x-large lheight20 margintop5')
 
     url = 'https://www.olx.pl/praca/informatyka/warszawa/q-javascript/'
-    headersJavascript = trade_spider(0, url, 'tr', 'wrap')
+    headers_javascript = trade_spider(0, url, 'tr', 'wrap')
 
     url = 'https://www.pracuj.pl/praca/JavaScript%20junior;kw/warszawa;wp/it%20-%20rozw%c3%b3j%20oprogramowania;cc,5016'
-    headersPracuj = pracuj.trade_pracuj_pl(0, url, 'h2', 'o-list_item_link')
+    headers_pracuj = pracuj.trade_pracuj_pl(0, url, 'h2', 'o-list_item_link')
 
     url = 'https://www.pracuj.pl/praca/front%20end%20junior;kw/warszawa;wp/it%20-%20rozw%c3%b3j%20oprogramowania;cc,5016'
-    headersPracujFront = pracuj.trade_pracuj_pl(0, url, 'h2', 'o-list_item_link')
+    headers_pracuj_front = pracuj.trade_pracuj_pl(0, url, 'h2', 'o-list_item_link')
 
     with open('parser.json') as readJson:
-        dataJson = json.load(readJson)
+        data_json = json.load(readJson)
 
-    headers = headersInterships + headersJavascript + headersPracuj + headersPracujFront
-    newHeaders = []
+    headers = headers_interships + headers_javascript + headers_pracuj + headers_pracuj_front
+    new_headers = []
 
     count = 0
     for i in range(len(headers)):
-        not_faund = True
+        not_found = True
 
-        for j in range(len(dataJson)):
-            if  headers[i]['text'] == dataJson[j]['text']:
-                not_faund = False
+        for j in range(len(data_json)):
+            if  headers[i]['text'] == data_json[j]['text']:
+                not_found = False
 
-        if not_faund == True:
-            newHeaders.append(headers[i])
+        if not_found == True:
+            new_headers.append(headers[i])
             triger = True
 
     if triger == True:
-        print (newHeaders)
+        print (new_headers)
         with open('parser.json', 'w') as file:
                 json.dump(headers, file, indent=2, ensure_ascii=False)
 
-        return newHeaders
+        return new_headers
--- TelegramBot.py
+++ TelegramBot.py
@@ -3,7 +3,9 @@ import OlxParser
 import time
 
 token = '385017512:AAHrISO0n6qaEcYbZq9utTdT8PqWLZw3Y7A'
-URL ='https://api.telegram.org/bot' + token + '/'
+URL ='https://api.telegram.org/bot'\
+     + token\
+     + '/'
 triger = False
 timer = 0
 
@@ -21,15 +23,16 @@ def get_message():
 
 def get_update_id():
     data = get_updates()
-    # first_update_id = data['result'][-2]['update_id']
     update_id = data['result'][-1]['update_id']
-    # if first_update_id == update_id:
-    #     return True
     return update_id
 
 
 def send_message(chat_id, text = 'jast a second...'):
-    url = URL + 'sendmessage?chat_id=' + str(chat_id) + '&text=' + text
+    url = URL + \
+          'sendmessage?chat_id=' \
+          + str(chat_id) \
+          + '&text=' \
+          + text
     print (url)
     requests.get(url)
 
@@ -40,11 +43,14 @@ if __name__ == "__main__":
     first_update_id = get_update_id()
 
     while True:
-        if timer == 5:
+        if timer == 600:
             new_heders = OlxParser.parser()
             if new_heders != None:
                 for i in range(len(new_heders)):
-                    message = '    ' + new_heders[i]['text'] + '   ' + new_heders[i]['link']
+                    message = '    ' \
+                              + new_heders[i]['text']\
+                              + '   ' \
+                              + new_heders[i]['link']
                     send_message(get_message()['chat_id'], message)
                     print (message)
             timer = 0
